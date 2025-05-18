d399a732adbeddf5694cad9c373f7e4afc0f6bf5
koenol <olli.s.koskinen@helsinki.fi>
2025-05-18 12:44:17 +0300

    added schema.sql check for install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,23 @@
+if pip install flask; then
+    echo "Flask installed."
+else
+    echo "Error: Could not execute pip install flask."
+    read -p "Possible causes: Pip is not installed correctly."
+fi
+
+if [ -f schema.sql ]; then
+    echo "schema.sql found"
+else
+    echo "Error: Could not locate schema.sql, unable to continue the installation."
+    read -p "Possible solution: Try to create database manually by running python install.py or python3 install.py. If that does not solve the problem, contact the developer."
+    exit 1
+fi
+
 if python install.py; then
+    echo "Database created succesfully"
     exit 0
 elif python3 install.py; then
+    echo "Database created succesfully"
     exit 0
 else
     echo "Error: Could not execute install.py using using python and python3."
