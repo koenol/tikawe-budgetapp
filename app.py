3298900cc69be5d6b2bf21184ef333353b1a9a2e
koenol <olli.s.koskinen@helsinki.fi>
2025-05-31 22:21:28 +0300

    added @app.route create

--- app.py
+++ app.py
@@ -14,3 +14,13 @@ def index():
 def register():
     render_template("register.html")
 
+@app.route("create", method=["POST"])
+def create():
+    username = request.form["username"]
+    password1 = request.form["password1"]
+    password2 = request.form["password2"]
+    if password1 != password2:
+        flash("VIRHE: Passwords do not match")
+        return redirect("/register")
+
+    return redirect("/")
