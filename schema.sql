caae4e6eab008a2d54c1c8686ae06207a70229a9
koenol <olli.s.koskinen@helsinki.fi>
2025-06-14 13:33:04 +0300

    added few extra NOT NULL to schema

--- schema.sql
+++ schema.sql
@@ -6,18 +6,18 @@ CREATE TABLE users (
 
 CREATE TABLE transactions (
     transaction_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    user_id INTEGER REFERENCES users,
+    user_id INTEGER REFERENCES users NOT NULL,
     amount INTEGER NOT NULL DEFAULT 0,
     transaction_type TEXT NOT NULL,
     transaction_message TEXT,
-    project_id INTEGER REFERENCES projects,
+    project_id INTEGER REFERENCES projects NOT NULL,
     date TEXT NOT NULL
 );
 
 CREATE TABLE projects (
     project_id INTEGER PRIMARY KEY,
     project_name TEXT NOT NULL,
-    project_owner_id INTEGER REFERENCES users,
+    project_owner_id INTEGER REFERENCES users NOT NULL,
     balance INTEGER NOT NULL DEFAULT 0,
     total_transactions INTEGER NOT NULL DEFAULT 0
 );
