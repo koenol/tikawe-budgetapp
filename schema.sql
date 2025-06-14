9df1753b6eeff276cfaf458a216d81e797a38aa2
koenol <olli.s.koskinen@helsinki.fi>
2025-06-14 13:24:51 +0300

    added schema references & autonincrement based on last week suggestions

--- schema.sql
+++ schema.sql
@@ -1,30 +1,30 @@
 CREATE TABLE users (
-    id INTEGER PRIMARY KEY,
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
     username TEXT UNIQUE,
     password_hash TEXT
 );
 
 CREATE TABLE transactions (
-    transaction_id INTEGER PRIMARY KEY,
-    user_id INTEGER,
+    transaction_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER REFERENCES users,
     amount INTEGER,
     transaction_type TEXT,
     transaction_message TEXT,
-    project_id INTEGER,
+    project_id INTEGER REFERENCES projects,
     date TEXT
 ); 
 
 CREATE TABLE projects (
-    project_id INTEGER PRIMARY KEY,
+    project_id INTEGER PRIMARY KEY AUTOINCREMENT,
     project_name TEXT,
-    project_owner_id INTEGER,
+    project_owner_id INTEGER REFERENCES users,
     balance INTEGER,
     total_transactions INTEGER
 );
 
 CREATE TABLE project_visibility (
-    user_id INTEGER,
-    project_id INTEGER,
+    user_id INTEGER REFERENCES users,
+    project_id INTEGER REFERENCES projects,
     view_permission BOOLEAN,
     edit_permission BOOLEAN
 );
